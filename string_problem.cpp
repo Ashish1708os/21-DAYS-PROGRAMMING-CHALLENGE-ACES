8cedaf6e88159ce3d73c14548d129ce25799d799
Ashish Salve <57785776+Ashish1708os@users.noreply.github.com>
2020-10-12 10:49:54 -0700

    DAY-8
    String problem (hackerRank)

---
+++ string_problem.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a = "abcd";
+    string b = "ef";
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<a+b<<endl;
+
+    string a1 = a;
+    string b1 = b;
+
+    a1[0] = b[0];
+    b1[0] = a[0];
+
+    cout<<a1<<" "<<b1<<endl;
+  
+    return 0;
+}
